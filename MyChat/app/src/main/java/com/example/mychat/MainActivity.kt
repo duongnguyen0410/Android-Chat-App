ac1307a2d62fb5cd0cbc433aafcd5653ab589d98
Nguyen Xuan Duong <128629368+duongnguyen0410@users.noreply.github.com>
2023-09-16 15:53:55 +0700

    update: add fragment navigation

--- MyChat/app/src/main/java/com/example/mychat/MainActivity.kt
+++ MyChat/app/src/main/java/com/example/mychat/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
 import com.example.mychat.login.LoginActivity
 import com.example.mychat.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -11,34 +12,27 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        replaceFragment(ChatFragment())
 
-        auth = Firebase.auth
-        val user = auth.currentUser
-        user?.let {
-            val name = it.displayName
-            val email = it.email
-            val photoUrl = it.photoUrl
-            val emailVerified = it.isEmailVerified
-            val uid = it.uid
-
-            Log.d(TAG, "user: $name, $email, $photoUrl, $emailVerified, $uid")
-        }
-
-        binding.btnSignOut.setOnClickListener {
-            signOut()
-            startActivity(Intent(this, LoginActivity::class.java))
-            finish()
+        binding.bottomNavigationView.setOnItemSelectedListener {
+             when(it.itemId){
+                 R.id.chat -> replaceFragment(ChatFragment())
+                 R.id.profile -> replaceFragment(ProfileFragment())
+            }
+            return@setOnItemSelectedListener true
         }
     }
 
-    private fun signOut(){
-        auth.signOut()
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 
     companion object {
