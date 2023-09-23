79013d41d664448c9931d870029e8169c7f45a3b
Nguyen Xuan Duong <128629368+duongnguyen0410@users.noreply.github.com>
2023-09-23 14:50:55 +0700

    update: insertUser() add: retrieveUser()

--- MyChat/app/src/main/java/com/example/mychat/UserRepository.kt
+++ MyChat/app/src/main/java/com/example/mychat/UserRepository.kt
@@ -12,17 +12,50 @@ class UserRepository {
     private val database = Firebase.database
     private val ref = Firebase.database.reference.child("users")
 
-    fun insertUser(user: FirebaseUser) {
+    fun insertUser(user: FirebaseUser, callback: (Boolean) -> Unit) {
         val newUser = User(user.uid, user.displayName.toString(), user.email.toString(), user.photoUrl.toString())
         ref.child(user.uid).setValue(newUser)
             .addOnCompleteListener {
                 if (it.isSuccessful){
                     Log.d(TAG, "insertUser: success")
-                    auth.signOut()
+                    callback(true)
                 } else {
                     Log.d(TAG, "insertUser: failed, ${it.exception}")
+                    callback(false)
                 }
             }
+    }
+
+    fun retrieveUser(uid: String, callback: (User) -> Unit){
+        ref.child(uid).get().addOnCompleteListener {
+            if (it.isSuccessful){
+                val user = it.result?.getValue(User::class.java)
+                if (user != null){
+                    callback(user)
+                }
+            } else {
+                Log.d(TAG, "retrieveUser: failed, ${it.exception}")
+            }
+        }
+    }
+
+    fun retrieveListUsers(callback: (ArrayList<User>) -> Unit){
+        val list = ArrayList<User>()
+        val currentUser = auth.currentUser
+        ref.get().addOnCompleteListener {
+            if (it.isSuccessful){
+                for (data in it.result.children){
+                    val user = data.getValue(User::class.java)
+                    if (user != null && user.uid != currentUser?.uid){
+                        list.add(user)
+                    }
+                }
+                callback(list)
+                Log.d(TAG, "retrieveListUsers: ${list.size}")
+            } else {
+                Log.d(TAG, "retreiveList: failed, ${it.exception}")
+            }
+        }
     }
 
     companion object {
